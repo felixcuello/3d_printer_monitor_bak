7ccb39d35155cfd653ddeb3645187b853b583d3a
Felix Cuello <felix.cuello@gmail.com>
2021-06-22 06:47:44 -0300

    crop and convert_to_bw are now functions

--- tools/video/tools.py
+++ tools/video/tools.py
@@ -16,12 +16,25 @@ def get_frames(output_directory, filename, frame_name, sample_rate_ms):
         if not success:
             break
 
-        height, width, channels = image.shape
-
-        image = image[0:height, 100:width-100]
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = crop_image(image, 15, 0)
+        image = convert_to_bw(image)
 
         cv2.imwrite( output_directory + "{}_{}.jpg".format(frame_name, count), image)     # save frame as JPEG file
         count = count + 1
 
     log_info("{} images read".format(count))
+
+# This function crops symmetrically left/right and top/bottom given a percentage
+def crop_image(image, x_percent, y_percent):
+    height, width, channels = image.shape
+
+    down = int(height * y_percent / 100)
+    up = height - down
+    left = int(width * x_percent / 100)
+    right = width - left
+
+    return image[down:up, left:right]
+
+# Just convert to black and white
+def convert_to_bw(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
